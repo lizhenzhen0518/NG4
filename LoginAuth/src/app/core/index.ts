04ce868042186869af4391888ff160d40b6e7ba0
lizhenzhen0518 <lizhenzhen0518@126.com>
2017-10-12 15:10:40 +0800

    add the LoginAuth

---
+++ LoginAuth/src/app/core/index.ts
@@ -0,0 +1,7 @@
+import { AppRoutingModule} from './app-routing.module';
+import { NgModule } from '@angular/core';
+@NgModule({
+  imports: [AppRoutingModule],
+  exports: [AppRoutingModule]
+})
+export class CoreModule {}
---
+++ LoginAuth/src/app/login/index.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ForgetComponent } from './forget';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { LoginRoutingModule } from './login-routing.module';
+import { SharedModule } from '../shared';
+@NgModule({
+  imports: [ LoginRoutingModule, SharedModule ],
+  declarations: [ForgetComponent , LoginComponent , RegisterComponent ]
+})
+export class LoginModule { }

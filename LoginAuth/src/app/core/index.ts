6c7b8348afa5c3531bd12105148ab4f732bb8bb4
lizhenzhen0518 <lizhenzhen0518@126.com>
2017-10-12 14:57:54 +0800

    delete the LoginAuth

--- LoginAuth/src/app/core/index.ts
+++
@@ -1,7 +0,0 @@
-import { AppRoutingModule} from './app-routing.module';
-import { NgModule } from '@angular/core';
-@NgModule({
-  imports: [AppRoutingModule],
-  exports: [AppRoutingModule]
-})
-export class CoreModule {}
